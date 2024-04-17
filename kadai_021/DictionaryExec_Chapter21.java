a5b9b6209721256aab3087de8c51bf4f127d5d44
takahashi-aicom <167046751+takahashi-aicom@users.noreply.github.com>
2024-04-17 14:18:41 +0900

    Add files via upload

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,14 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+    public static void main(String[] args) {
+        Dictionary_Chapter21 sercWord = new Dictionary_Chapter21();
+
+        sercWord.fruitDic();
+
+        sercWord.setSearchWord("banana");
+
+        sercWord.searchDic();
+    }
+}
---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,35 @@
+package kadai_021;
+
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+    HashMap<String, String> dictionary = new HashMap<String, String>();
+
+    String givenWord;
+
+    public void fruitDic() {
+    	dictionary.put("apple", "りんご");
+    	dictionary.put("peach", "桃");
+    	dictionary.put("banana", "バナナ");
+    	dictionary.put("lemon", "レモン");
+    	dictionary.put("pear", "梨");
+    	dictionary.put("kiwi", "キウィ");
+    	dictionary.put("strawberry", "いちご");
+    	dictionary.put("grape", "ぶどう");
+    	dictionary.put("muscat", "マスカット");
+    	dictionary.put("cherry", "さくらんぼ");
+    }
+
+    public void setSearchWord(String givenWord) {
+        this.givenWord = givenWord;
+    }
+
+    public void searchDic() {
+        if(dictionary.containsKey(givenWord)) {
+            String meaning = dictionary.get(givenWord);
+            System.out.println(givenWord + "の意味は " + meaning + " です。");
+        } else {
+            System.out.println(givenWord + "は辞書に存在していません。");
+        }
+    }
+}
